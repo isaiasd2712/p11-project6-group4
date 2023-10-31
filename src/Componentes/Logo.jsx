44e69a56e42f5f643d82aff91c7bc6083d2e6c58
AndresOcampoFactoriaF5 <less.broken.hearts@outlook.com>
2023-11-01 00:47:04 +0100

    Borrar contenido de la rama main

--- src/Componentes/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import './Logo.css';
-import logo from '../assets/logo.png';
-
-function Logo() {
-    return (
-        <img className="aura" src={logo} alt="MILOGO" />
-    )
-}
-export default Logo;
