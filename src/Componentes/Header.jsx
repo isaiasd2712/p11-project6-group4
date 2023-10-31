773ca5694e9f82770b6d9b808c1e809a99015884
isaiasd2712 <isaiasd2712@gmail.com>
2023-10-31 09:10:38 +0100

    inicio

---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header () {
+    return (
+        <div className='header-left'>
+        <h1>Piratify</h1>
+        </div>
+        
+    )
+}
+export default Header;
